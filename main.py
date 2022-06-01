6754af274b86c99ac0f8b6de0b4136f18080908b
ranshaa05 <ranshaa05@gmail.com>
2022-06-01 20:57:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import os
 from natsort import natsorted
 
 print("Select folder of reference pictures:")
-reference_dir = filedialog.askdirectory(title="Select folder", mustexist=True)
+reference_dir = filedialog.askdirectory(title="Select reference folder", mustexist=True)
 print(reference_dir)
 known_images = natsorted(os.listdir(reference_dir))
 
 print("select folder to compare against reference:")
-comparison_dir = filedialog.askdirectory(title="Select folder", mustexist=True)
+comparison_dir = filedialog.askdirectory(title="Select destination folder", mustexist=True)
 print(comparison_dir)
 files_to_compare = natsorted(os.listdir(comparison_dir), key=lambda y: y.lower())
 
 
 named_encodings = {}
-loading_progress = 1
+loading_progress = 0
 
 for i in known_images:  #load and encode all known images
     if not "." in i or i.split(".")[1] not in ["jpg", "jpeg", "png", "webp"]:
@@ -28,14 +28,20 @@ for i in known_images:  #load and encode all known images
     if len(known_encoding) >1:
         print(f"File '{i}' has multiple faces in it. Please use a different file with only one face.")
         exit()
-        
-    named_encodings[name] = known_encoding
+    
+    if not len(known_encoding) == 0:
+        named_encodings[name] = known_encoding
+    else:
+        input(f"Could find a face in file '{i}'. Press enter to continue.")
     loading_progress += 1
     print(f"{loading_progress}/{len(known_images)} ({round(loading_progress / len(known_images) * 100)}%) reference images loaded.\r", end="")
 
 print("")
+
 catagorize_progress = 1
 num_of_catagorized_faces = 0
+existing_filenames = {} #to avoid overwriting files and keep track of how many pics of with the same people have been catagorized.
+
 for file in files_to_compare:
     if not "." in file or file.split(".")[1] not in ["jpg", "jpeg", "png", "webp"]: #if not image file
         continue
@@ -44,7 +50,7 @@ for file in files_to_compare:
     unknown_image = fr.load_image_file(file_path)
     unknown_encodings = fr.face_encodings(unknown_image)
     recognized_people = []
-
+    
     if unknown_encodings:
         unchecked_faces = len(unknown_encodings)
         # print(f"{unchecked_faces} face(s) found in image")
@@ -57,16 +63,22 @@ for file in files_to_compare:
                 # print(f"Found {name}.\n")
                 recognized_people.append(name) #add name to list of recognized people in image
                 unchecked_faces -= 1
-            
 
         if recognized_people:
             recognized_people = str(recognized_people).strip("[]'").replace("'", "")
-            for i in range(len(files_to_compare)):  #TODO: the more files there are to check, the less efficient this is. find a better way to do this.
-                try:
-                    os.rename(file_path, f"{comparison_dir}/{recognized_people} {i+1}{os.path.splitext(file)[1]}")
-                    break
-                except FileExistsError:
-                    pass
+            if recognized_people in existing_filenames:
+                existing_filenames[recognized_people] += 1
+            else:
+                existing_filenames[recognized_people] = 1
+
+            while os.path.isfile(f"{comparison_dir}/{recognized_people} {existing_filenames[recognized_people]}{os.path.splitext(file)[1]}"):
+                existing_filenames[recognized_people] += 1 #make sure this filename is available
+            try:
+                os.rename(file_path, f"{comparison_dir}/{recognized_people} {existing_filenames[recognized_people]}{os.path.splitext(file)[1]}")
+            except FileExistsError:
+                print(f"File '{file}' already exists. Skipping...") #just in case
+                continue
+                 #TODO: does not account for files that are already in the folder with the same name or files that have been renamed while the program is running.
             num_of_catagorized_faces += 1
                 
     else:
